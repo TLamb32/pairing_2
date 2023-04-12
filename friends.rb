1b3fb1c8cc3b6d0f67e4dae4d0ac4dff8a81199d
Terry Lamb <terrylamb@gmail.com>
2023-04-12 17:34:50 -0400

    Add places array

--- friends.rb
+++ friends.rb
@@ -2,3 +2,7 @@ arrays = ["Friend1", "Friend2" , "Friend3" , "Friend4" , "Friend5"]
 puts arrays [2]
 puts arrays [4]
 puts arrays [0]
+
+places = ["place1" , "place2" , "place3" , "place4" , "place5"]
+places.pop
+puts places
